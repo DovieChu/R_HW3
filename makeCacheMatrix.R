136d7ecceeeabed28582bcacc35fc000c8841fd0
DovieChu <chuge1993@yeah.net>
2015-05-24 12:31:00 +0800

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -11,3 +11,18 @@ makeCacheMatri<-function(x=matrix()){
   getSolve<-function()m
   list(set=set,get=get,setmean=setmean,getmean=getmean)
 }
+
+
+
+cacheSolve<-function(x,...){
+  m<-x$getSolve()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  #Judge that if m is null and return to the certain message
+  data<-x$get()
+  m<-solve(data,...)
+  x$setSolve(m)
+  m
+}
